4179fedbbd9528c372a8e3b6f6908d4d8064548a
denywa <dwa1503@gmail.com>
2024-12-06 01:33:09 +0800

    Create first_screen.dart

---
+++ lib/first_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/second');
+          },
+          child: Text('Go to Second Screen'),
+        ),
+      ),
+    );
+  }
+}
